24078e4bce2214b304ea0acf3b88af6c4947ab04
Fady Zaki <fz20@st-andrews.ac.uk>
2017-05-04 15:22:14 +0100

    change database details

--- server_side/build_database.php
+++ server_side/build_database.php
@@ -2,21 +2,26 @@
 	include("connect.php");
 	error_reporting(-1);
 	ini_set('display_errors', 'On');
-	createUserTable();
+	dropUserTable();
+  createUserTable();
   createFoldersTable();
   createReferencesTable();
 
-	//Create user table in the database
+	function dropUserTable() {
+    $db -> dropTable("user");
+  }
+
+  //Create user table in the database
 	function createUserTable() {
 	    global $db;
 	    $creatUserTableSql = "CREATE TABLE IF NOT EXISTS user (
-		id int(11) NOT NULL AUTO_INCREMENT,
-		uname varchar(50) NOT NULL,
-		hash varchar(200) NOT NULL,
-		user_bio varchar(300) NOT NULL,
+		id int(6) NOT NULL AUTO_INCREMENT,
+		uname varchar(30) NOT NULL,
+		hash varchar(100) NOT NULL,
+		user_bio varchar(100) NOT NULL,
 		PRIMARY KEY (id),
 		UNIQUE KEY uname (uname)
-		)";
+		);";
 
 		if($db->createTable($creatUserTableSql))
 			echo "<h3>Table User created.</h3>";
@@ -26,11 +31,12 @@
   function createFoldersTable() {
       global $db;
       $creatFolderTableSql = "CREATE TABLE IF NOT EXISTS folders(
-          id INT(11) NOT NULL AUTO_INCREMENT,
+          id INT(6) NOT NULL AUTO_INCREMENT,
           name VARCHAR(45) DEFAULT NULL,
-          user_id INT(11) NOT NULL,
+          user_id INT(6) NOT NULL,
           PRIMARY KEY (id),
-          FOREIGN KEY (user_id) REFERENCES user(id))";
+          FOREIGN KEY (user_id) REFERENCES user(id)
+          )";
 
       if($db->createTable($creatFolderTableSql))
       	echo "<h3>Table Folder created.</h3>";
@@ -62,7 +68,7 @@
           publisher VARCHAR(100) DEFAULT NULL,
           PRIMARY KEY (id),
           UNIQUE KEY pdf_url_unique (pdf_url),
-          FOREIGN KEY (folder_id) REFERENCES folders(id))";
+          FOREIGN KEY (folder_id) REFERENCES folders(id));";
 
       if($db->createTable($createReferencesTableSql))
         echo "<h3>Table References created.</h3>";
